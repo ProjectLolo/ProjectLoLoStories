b30304313e7a02693e9f1ec1a3d057e9e35fab7b
Andre2610 <dre2610@gmail.com>
2020-11-12 16:10:35 +0100

    added a findAll endpoint

--- router/story.js
+++ router/story.js
@@ -1,3 +1,23 @@
+const { Router } = require("express");
+
 const router = new Router();
+const Story = require("../models").story;
+const Page = require("../models").page;
+
+router.get("/", async (req, res, next) => {
+  try {
+    const getStories = await Story.findAll({
+      include: Page,
+    });
+
+    if (!getStories) {
+      res.status(404).send("No stories found");
+    } else {
+      res.json(getStories);
+    }
+  } catch (e) {
+    next(e);
+  }
+});
 
 module.exports = router;
